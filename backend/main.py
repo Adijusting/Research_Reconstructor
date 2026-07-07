c06f55e587bc3abcb08d3821450a6b4f5f336903
unknown <deshpandeaditya806@gmail.com>
2026-07-08 00:50:17 +0530

    Completely replaced old project files with new files

--- backend/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI(title="Research Reconstruction API")
-
-
-@app.get("/")
-def read_root():
-    return {"status":"Environment is successfully configured!", "system":"Online"}
